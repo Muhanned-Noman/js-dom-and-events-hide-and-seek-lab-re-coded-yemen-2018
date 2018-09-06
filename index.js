04e412e75242200d788c97bd2c962b20d1374fc0
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-09-06 18:37:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function increaseRankBy(n){
 
     for(let x = 0; x < listElement.length ; x++){
       listElement.innerHTML = parseInt(listElement[x].innerHTML) + n;
-    };
-  };
+    }
+  }
 };
