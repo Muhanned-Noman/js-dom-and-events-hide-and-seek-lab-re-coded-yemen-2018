cab7dda66fdb38194de487251a0a33c0f718e447
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-09-06 18:25:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+};
+function nestedTarget(){
+  return document.getElementById('#nested .target');
+};
+function increaseRankBy(n){
+  return parseInt(document.getElementsByClassName(.ranked-list).inerHTML(),n);
+};
